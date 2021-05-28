dd2816c1ff4588a7d256539f7c78932458c39947
Margaux83 <margauxhebert83@gmail.com>
2021-05-28 18:51:17 +0200

    Ajout d'élément de style dans la page combat

--- src/Combat.js
+++ src/Combat.js
@@ -1,17 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import './Combat.css';
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Pokemon from "./Pokemon";
 
+
+function MonsterBlock(){
+    return(
+        <>
+            <div className="row">
+                <div className="card col mb-4 center">
+                <p style={{fontSize:25}}>Monstre Zelda</p>
+                </div>
+                <br/><br/><br/>
+                <div className="card col mb-4 center" >
+                <p style={{fontSize:25}}>Monstre Pokémon</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+function BeginCombat(){
+
+    return (
+        <>
+            <div className="containerButton h-100">
+                        <button className="buttonBeginCombat"  type={"submit"} >Lancer un combat</button>
+            </div>
+
+            <div className="monsterBlock text-center">
+                <MonsterBlock></MonsterBlock>
+            </div>
+        </>
+    )
+}
 
 function Combat() {
 
     return (
-        <div className="App">
-            <header className="App-header">
-
-            </header>
-        </div>
+                <BeginCombat></BeginCombat>
     );
 }
 
